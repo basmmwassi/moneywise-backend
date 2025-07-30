a8f19fd1bf44ce84b0474dd65707c6edefb856da
basm <basmmwassi100@gmail.com>
2025-07-31 01:33:00 +0300

    Add files via upload

--- routes/depositRoutes.js
+++ routes/depositRoutes.js
@@ -3,28 +3,8 @@ const router = express.Router();
 const { createDeposit, getTotalDeposits } = require('../controllers/depositController');
 const { protect } = require('../middleware/authMiddleware');
 
-
-
 router.get('/total', getTotalDeposits);
 
-
-router.post('/', protect, async (req, res) => {
-  try {
-    const { amount, source } = req.body;
-    const userId = req.user.id;
-
-    const newDeposit = new Deposit({
-      userId,
-      amount,
-      source
-    });
-
-    await newDeposit.save();
-    res.status(201).json(newDeposit);
-  } catch (err) {
-    res.status(500).json({ message: 'Error creating deposit', error: err.message });
-  }
-});
-
+router.post('/', protect, createDeposit);
 
 module.exports = router;
