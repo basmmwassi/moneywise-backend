13a2b24a1ff8bef79cb413c66d3add7875949583
basm <basmmwassi100@gmail.com>
2025-07-31 08:09:06 +0300

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,8 @@ const User = require('../models/User');
 
 const deleteMyAccount = async (req, res) => {
   try {
-    const { accountCode } = req.body;
-
     const user = await User.findById(req.user.id);
     if (!user) return res.status(404).json({ message: 'User not found' });
-
-    if (user.accountCode !== accountCode) {
-      return res.status(400).json({ message: 'Invalid account code' });
-    }
 
     await User.findByIdAndDelete(req.user.id);
     res.status(200).json({ message: 'Your account has been deleted successfully' });
@@ -17,6 +11,5 @@ const deleteMyAccount = async (req, res) => {
     res.status(500).json({ message: 'Error deleting account', error: error.message });
   }
 };
-
 
 module.exports = { deleteMyAccount };
