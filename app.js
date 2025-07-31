c9a93030763867e1ea716ae362b4518bc94566d0
basm <basmmwassi100@gmail.com>
2025-07-31 21:08:48 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://money-wise1.netlify.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use('/api', transactionRoutes);
